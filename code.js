7e40abf38d8a946e63bde500cd51b29f808ee53f
Danny Larrea <danny@MacBook-Air-de-Danny.local>
2020-11-20 18:49:20 +0100

    subida 20 nov. 2020

--- code.js
+++ code.js
@@ -1,18 +1,26 @@
 window.onload = function() {
     document.getElementById('add-input').addEventListener("click", addInput);
+    document.getElementById('del-input').addEventListener("click", delInput);
+    document.getElementById('dni').addEventListener("focusout", validarDNI);
+    document.getElementById('nac').addEventListener("focusout", categoria)
+        //document.getElementById('form').addEventListener('submit', validarForm);
 }
 
 /* addInput */
 function addInput(event) {
+
     if (document.getElementsByClassName('num').length != 10) {
         var numAux = 'valor-' + (document.getElementsByClassName('num').length + 1);
         var div = document.createElement('div');
         var label = document.createElement('label');
         var input = document.createElement('input');
 
+        div.setAttribute('class', 'input')
+
         /* label */
         label.setAttribute('for', numAux)
         label.innerHTML = 'Valor ' + (document.getElementsByClassName('num').length + 1) + ': ';
+
         /* input */
         input.setAttribute('type', 'number');
         input.setAttribute('name', numAux);
@@ -30,10 +38,46 @@ function addInput(event) {
 }
 
 
-/* delInput */
+/* delInput 
+var x = document.getElementById("myList").lastChild.innerHTML;*/
+function delInput(event) {
+    if (document.getElementsByClassName('num').length != 2) {
+        var inputList = document.getElementsByClassName('input');
+        console.log('tamaño del vector: ' + inputList.length);
+        // inputList=[0, 1, 2] tamaño 3
+        // inputList[2]
+        inputList[(inputList.length - 1)].remove();
+    }
+    event.preventDefault();
+}
 
-/* validarForm */
+/* validarForm: mirar si hay campos vacios */
 
 /* validarDNI */
+function validarDNI() {
 
+    var dni = document.getElementById('dni');
+    if (dni.value.length == 9) {
+        //console.log('DNI válido');
+        dni.style.border = "solid green 5px";
+    } else {
+        dni.style.border = "solid red 5px";
+    }
+
+}
 /* Mostrar categoría a partir de la fecha de nacimiento */
+function categoria() {
+    var nacimiento = document.getElementById('nac').value;
+    var nac = new Date(nacimiento);
+    var msg = document.getElementById('msg');
+    var fecha = new Date();
+
+    console.log(nac);
+    console.log(fecha);
+
+    // si la fecha de nacimiento es inferior a 30 de nov. 2020 va a indicarnos que somos viejos
+    if (nac < fecha) {
+        msg.innerHTML = 'somos viejos';
+    }
+
+}
