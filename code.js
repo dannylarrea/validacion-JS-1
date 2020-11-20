4cf66d4db08590404e9ec1ec13edda2394dd84e5
Danny Larrea <danny@MacBook-Air-de-Danny.local>
2020-11-20 16:23:25 +0100

    20 de nov. 2020

--- code.js
+++ code.js
@@ -1,13 +1,39 @@
-function validarFormulario() {
-
+window.onload = function() {
+    document.getElementById('add-input').addEventListener("click", addInput);
 }
 
-function addInput() {
-    var inputs = document.getElementsByClassName('num').length + 1;
-    var div = document.createElement('div');
-    div.innerHTML = '<label for="">Valor ' + inputs + ':</label> <input type="number" name="valor-' + inputs + '" id="valor-' + inputs + '" class="num">';
-    document.getElementById('values').append(div);
-    //alert(document.getElementById('form-values').innerHTML)
-    return false;
+/* addInput */
+function addInput(event) {
+    if (document.getElementsByClassName('num').length != 10) {
+        var numAux = 'valor-' + (document.getElementsByClassName('num').length + 1);
+        var div = document.createElement('div');
+        var label = document.createElement('label');
+        var input = document.createElement('input');
 
+        /* label */
+        label.setAttribute('for', numAux)
+        label.innerHTML = 'Valor ' + (document.getElementsByClassName('num').length + 1) + ': ';
+        /* input */
+        input.setAttribute('type', 'number');
+        input.setAttribute('name', numAux);
+        input.setAttribute('id', numAux);
+        input.setAttribute('class', 'num');
+
+        /* anidamos etiquetas */
+        div.appendChild(label)
+        div.appendChild(input)
+
+        //div.innerHTML = '<label for="">Valor ' + inputs + ':</label> <input type="number" name="valor-' + inputs + '" id="valor-' + inputs + '" class="num">';
+        document.getElementById('values').appendChild(div);
+    }
+    event.preventDefault();
 }
+
+
+/* delInput */
+
+/* validarForm */
+
+/* validarDNI */
+
+/* Mostrar categoría a partir de la fecha de nacimiento */
